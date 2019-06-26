a83ce3e8d269127b570c51f27714567b8f935214
Intern426 <52214834+Intern426@users.noreply.github.com>
2019-06-26 15:22:56 -0700

    Options (#7)
    * Added a Scanner to read user input.
    Created a switch statement to check user input.
    Created functions for each task in the menu.
    
    * Bulletproofed the user's input regarding their menu options.
    Will only accept 1-5 with the generic message "Incorrect Entry. Please try again."
    
    * Added one more bulletproof for empty values
    
    * Fixed the style
    
    * Fixed a redundant initialization with the choice variable

--- src/main/java/com/azure/app/App.java
+++ src/main/java/com/azure/app/App.java
@@ -3,6 +3,8 @@
 
 package com.azure.app;
 
+import java.util.Scanner;
+
 /**
  * A library application that keeps track of books using Azure services.
  */
@@ -13,12 +15,35 @@ public class App {
      * @param args Arguments to the library program.
      */
     public static void main(String[] args) {
-        showMenu();
+        System.out.print("Welcome! ");
+        Scanner sc = new Scanner(System.in);
+        int choice;
+        do {
+            showMenu();
+            String option = sc.nextLine();
+            choice = checkOption(option);
+            switch (choice) {
+                case 1:
+                    listBooks();
+                    break;
+                case 2:
+                    addBook();
+                    break;
+                case 3:
+                    findBook();
+                    break;
+                case 4:
+                    deleteBook();
+                    break;
+                default:
+                    System.out.println("Please try again.");
+            }
+        } while (choice != 5);
+
     }
 
-
     private static void showMenu() {
-        System.out.println("Welcome! Select one of the options  below (1 - 5).");
+        System.out.println("Select one of the options  below (1 - 5).");
         System.out.println("1. List books");
         System.out.println("2. Add a book");
         System.out.println("3. Find a book");
@@ -26,4 +51,34 @@ public class App {
         System.out.println("5. Quit");
     }
 
+    private static void listBooks() {
+        System.out.println("Here are your list of books:");
+    }
+
+    private static void addBook() {
+        System.out.println("Please enter the following information:");
+    }
+
+    private static void findBook() {
+        System.out.println("How would you like to find the book?");
+    }
+
+    private static void deleteBook() {
+        System.out.println("Enter the title of the book to delete: ");
+    }
+
+    private static int checkOption(String option) {
+        int choice = -1;
+        if (option.isEmpty()) {
+            System.out.println("Please enter a value.");
+        }
+        try {
+            choice = Integer.parseInt(option);
+        } catch (NumberFormatException ex) {
+            System.out.print("Please enter a numerical value. ");
+        }
+        return choice;
+    }
 }
+
+
