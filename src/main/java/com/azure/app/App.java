bf7aefeb7514849ed6552b69996f2a5ec48d0611
Intern426 <52214834+Intern426@users.noreply.github.com>
2019-06-26 11:34:31 -0700

    Feature1 (#4)
    * Added for loop
    
    * Added goodbye

--- src/main/java/com/azure/app/App.java
+++ src/main/java/com/azure/app/App.java
@@ -14,5 +14,9 @@ public class App {
      */
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        for (int i = 1; i < 11; i++) {
+            System.out.println(i);
+        }
+        System.out.println("Goodbye");
     }
 }
