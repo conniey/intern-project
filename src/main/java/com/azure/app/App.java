a3ffd974ae249de0db94ab147f2e163a8f35bb3e
Connie <conniey@microsoft.com>
2019-06-26 08:47:03 -0700

    Fixing checkstyle issues.

--- src/main/java/com/azure/app/App.java
+++ src/main/java/com/azure/app/App.java
@@ -1,13 +1,18 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 package com.azure.app;
 
 /**
- * Hello world!
- *
+ * A library application that keeps track of books using Azure services.
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    /**
+     * Starting point for the library application.
+     *
+     * @param args Arguments to the library program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
