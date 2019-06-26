4d17391abba27af5bd034b8fb9602629285587bd
Connie Yau <conniey@microsoft.com>
2019-06-26 09:24:24 -0700

    Add maven support, checkstyles. (#2)
    * Adding initial project
    
    * Adding more git ignores.
    
    * Adding checkstyle rules.
    
    * Remove custom azure checkstyles

---
+++ src/main/java/com/azure/app/App.java
@@ -0,0 +1,18 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.azure.app;
+
+/**
+ * A library application that keeps track of books using Azure services.
+ */
+public class App {
+    /**
+     * Starting point for the library application.
+     *
+     * @param args Arguments to the library program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/com/azure/app/AppTest.java
@@ -0,0 +1,36 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.azure.app;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest extends TestCase {
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest(String testName) {
+        super(testName);
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite() {
+        return new TestSuite(AppTest.class);
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp() {
+        assertTrue(true);
+    }
+}
