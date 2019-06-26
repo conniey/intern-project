33b71db417e5f603b78a905922b4f803c56741fd
Intern456 <ayelechtadesse@gmail.com>
2019-06-26 11:23:13 -0700

    Added for loop

--- src/main/java/com/azure/app/App.java
+++ src/main/java/com/azure/app/App.java
@@ -14,5 +14,8 @@ public class App {
      */
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        for (int i = 1; i < 11; i++) {
+            System.out.println(i);
+        }
     }
 }
