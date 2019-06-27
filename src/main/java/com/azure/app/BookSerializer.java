6c529cbec888319b6c69e1ee3f2abc26e4dcc72d
Intern456 <ayelechtadesse@gmail.com>
2019-06-27 14:08:26 -0700

    Formatted the json file so that every data member is on its own line

--- src/main/java/com/azure/app/BookSerializer.java
+++ src/main/java/com/azure/app/BookSerializer.java
@@ -1,6 +1,7 @@
 package com.azure.app;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 
 import java.io.File;
 import java.io.IOException;
@@ -14,6 +15,7 @@ public class BookSerializer {
             File dir2 = new File(dir.getAbsolutePath() + "\\" + book.getFirstName());
             dir2.mkdir();
             ObjectMapper mapper = new ObjectMapper();
+            mapper.enable(SerializationFeature.INDENT_OUTPUT);
             mapper.writeValue(new File(dir2.getAbsolutePath() + "\\" + book.getTitle() + ".json"), book);
         } catch (IOException ex) {
             System.out.println("Error in File Handling. Did not save.");
