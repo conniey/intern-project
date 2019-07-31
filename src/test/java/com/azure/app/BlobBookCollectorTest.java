d186d0d50880fb344c2471a2370cc2a3d71bf127
Intern456 <ayelechtadesse@gmail.com>
2019-07-31 13:40:51 -0700

    Merged Cosmos in Fixed the BlobTest

--- src/test/java/com/azure/app/BlobBookCollectorTest.java
+++ src/test/java/com/azure/app/BlobBookCollectorTest.java
@@ -1,16 +1,48 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 package com.azure.app;
 
+import com.azure.core.http.policy.HttpLogDetailLevel;
+import com.azure.data.appconfiguration.ConfigurationAsyncClient;
+import com.azure.data.appconfiguration.credentials.ConfigurationClientCredentials;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
 
 import java.io.File;
 import java.net.URL;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 
 public class BlobBookCollectorTest {
-    private BlobBookCollector blobCollector = new BlobBookCollector();
+    private BlobBookCollector blobCollector;
     URL folder = LocalBookCollectorTest.class.getClassLoader().getResource(".");
+
+    /**
+     * Set up the App Configuration to grab the information for the Blob Storage
+     */
+    @Before
+    public void setup() {
+        String connectionString = System.getenv("AZURE_APPCONFIG");
+        if (connectionString == null || connectionString.isEmpty()) {
+            System.err.println("Environment variable AZURE_APPCONFIG is not set. Cannot connect to App Configuration."
+                + " Please set it.");
+            return;
+        }
+        ConfigurationAsyncClient client;
+        try {
+            client = ConfigurationAsyncClient.builder()
+                .credentials(new ConfigurationClientCredentials(connectionString))
+                .httpLogDetailLevel(HttpLogDetailLevel.HEADERS)
+                .build();
+            blobCollector = new BlobBookCollector(client);
+        } catch (NoSuchAlgorithmException | InvalidKeyException e) {
+            Assert.fail("");
+        }
+    }
 
     /**
      * Checks that saving a book works
@@ -92,7 +124,6 @@ public class BlobBookCollectorTest {
             .verify();
     }
 
-
     /**
      * Tests saving a two different books by the same author AND with the same cover
      */
@@ -116,7 +147,6 @@ public class BlobBookCollectorTest {
             .expectComplete()
             .verify();
     }
-
 
     /**
      * Tests overwriting the same book but with a different cover image
